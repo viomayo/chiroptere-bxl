1b47c6d5d620ece30645ad3a6737fac8e038e611
viomayo <viomayo@gmail.com>
2026-06-04 23:41:19 +0200

    chore: initialisation de la connexion a Supabase

---
+++ proxy.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@/utils/supabase/middleware'
+import type { NextRequest } from 'next/server'
+
+export async function proxy(request: NextRequest) {
+  const { supabase, supabaseResponse } = createClient(request)
+  await supabase.auth.getUser()
+  return supabaseResponse
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+}
